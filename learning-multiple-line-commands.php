5b38732421f3bdb6ae29ed6e48be55902e068291
Ian Hulston <hulstonian@gmail.com>
2019-02-13 22:48:12 +0000

    Started writing out the notes and code for multiple line commands.

---
+++ learning-multiple-line-commands.php
@@ -0,0 +1,25 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<title>Multiple Line Comands - Beginning</title>
+</head>
+<body>
+	<h1>Multiple Line Commands</h1>
+
+		<p>When we need to output quite alot of text from PHP, using several echo and print statments would be time consuming and messy.</p>
+		<p><strong>PHP</strong> offers two conveniences to overcome the numerous echo and print statements.</p></p>
+
+	<hr>
+
+		<p>PHP also offers a <strong>multiline sequence</strong> using the</p>
+
+		<p><<< operator - commonly referred to as a <i>here document</i> or <i>heredoc</i> as a way of specifying a <strong>string literal</strong>, preserving the line breaks and other whitespace</p>
+		<p>(including indetation) in a text.</p>
+
+		<p>Example of it's use;</p>
+
+		<?php
+			$author = "Ian Hulston";
+		?> 
+</body>
+</html>
