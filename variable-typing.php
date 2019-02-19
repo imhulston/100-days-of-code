e1742d5d4d27c3630cc3cbd9c7c5ae227ddcdd3e
Ian Hulston <hulstonian@gmail.com>
2019-02-19 23:00:59 +0000

    Adding notes and pratice examples of the substr function.

--- variable-typing.php
+++ variable-typing.php
@@ -18,7 +18,25 @@ echo
 _END;
 
 $number = 12345 * 67890;
-echo substr($number, 3, 1); // Automatic conversion from a number to a string.
+echo substr($number, 3, 1); // Automatic conversion from a number to a string. 
+// The output on the webpage will display one.
+// 
+// 
+echo "<br><br>";
+echo '$number has been declared as a number variable with the number value coming from multiplying 12345 by 67890.<br>';
+echo 'Then using the substr [substring] function to extract character from the $number value and display it as a string even though it\'s a number';
 	?>
+<br><br><br>
+
+<?php
+echo '<h2>Practice of the substr() function<?h2><br>';
+echo '<br>';
+$text = 'I want to break free';
+echo substr($text, 0, 6); //extracting and displaying the 6 characters from the beginning of the string.
+echo '<br>';
+echo substr($text, 10, 10); // Starting from the 10th chararcter and then extracting the next 10 characters.
+echo '<br>';
+echo substr($text, -5, 5); // Starting from the end of the string and going 5 characters back and then extracting the next 5 characters which will display 'free'.
+?>
 </body>
 </html>
