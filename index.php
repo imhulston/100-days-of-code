8fcdd00981a60bf999f78e8b954db63b0c2dab37
Ian Hulston <hulstonian@gmail.com>
2019-03-06 22:34:49 +0000

    Created basic layout with mobile view intended.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html> <!-- HTML 5 -->
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home Page</title>
+	<link rel="stylesheet" href="styles/main.css">
+	<link rel="stylesheet" href="styles/mobile.css">
+</head>
+<body>
+	<header>
+		<h1>Hello, I'm Ian</h1>
+	</header>
+	<div id="top-nav" class="navigation">
+		<span id="closebtn" onclick="navToggle()">
+			<span class="line1"></span>
+			<span class="line2"></span>
+			<span class="line3"></span>
+		</span>
+		<div class="logo">
+			<img src="">
+		</div>
+		
+		<ul class="menulist"> 
+			<li><a class="menuitems" href="index.php">Home</a></li>
+			<li><a class="menuitems" href="profile.php">Profile</a></li>
+			<li><a class="menuitems" href="projects.php">Projects</a></li>
+			<li><a class="menuitems" href="cv.php">CV</a></li>
+			<li><a class="menuitems" href="contact.php">Contact</a></li>
+		</ul>
+	</div>
+
+	<section>
+		<img src="">
+		<h2>How can I help?</h2>
+	</section>
+
+	<div id="project-container"> <!-- Finger swiping carousel displaying latest projects with description. -->
+		
+	</div>
+
+	<div id="about-me-container"> <!-- This container to be red coloured -->
+		
+	</div>
+
+	<footer>
+		<div id="footer-links">
+			<p>Ian Hulston</p>
+			<p><a href="mailto:hulstonian@gmail.com"> Email me</a></p>
+		</div>
+	</footer>
+</body>
+</html>
